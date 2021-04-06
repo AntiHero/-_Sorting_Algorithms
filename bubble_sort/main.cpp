9c86ce986d1cd47ae3f2018007a9baa31a6d2a53
Aleksandr Schemelev <prophreaker@gmail.com>
2021-04-06 16:25:00 +0300

    refactor

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -3,43 +3,37 @@
 #include <string>
 #include "../utils/utils.h"
 
-void bubble_sort(std::vector<int> &V)
-{
-  bool sorted_flag = false;
-  int bypass_counter = 0;
+void bubble_sort(std::vector<int> &V) {
+    std::cout << "bubble sort" << std::endl;
 
-  while (not sorted_flag)
-  {
-    sorted_flag = true;
-    for (int i = 0; i < int(V.size()) - 1 - bypass_counter; i++)
-    {
-      if (V[i] > V[i + 1])
-      {
-        std::swap(V[i], V[i + 1]);
-        sorted_flag = false;
-      }
+    bool sorted_flag = false;
+    int bypass_counter = 0;
+
+    while (not sorted_flag) {
+        sorted_flag = true;
+        for (int i = 0; i < int(V.size()) - 1 - bypass_counter; i++) {
+            if (V[i] > V[i + 1]) {
+                std::swap(V[i], V[i + 1]);
+                sorted_flag = false;
+            }
+        }
+        bypass_counter++;
+        print_vector(V);
     }
-    bypass_counter++;
-    print_vector(V);
-  }
 }
 
-int main()
-{
-  std::vector<int> V;
+int main() {
+    std::vector<int> V;
 
-  V = input_vector();
-  print_vector(V);
-  bubble_sort(V);
+    V = input_vector();
+    print_vector(V);
+    bubble_sort(V);
 
-  if (is_ascending_order(V))
-  {
-    std::cout << "ascending\n";
-  }
-  else
-  {
-    std::cout << "not ascending";
-  }
+    if (is_ascending_order(V)) {
+        std::cout << "ascending\n";
+    } else {
+        std::cout << "not ascending";
+    }
 
-  return 0;
+    return 0;
 }
