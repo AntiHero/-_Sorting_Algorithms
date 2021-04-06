06b5fd3ccdf29d6da81bdc92b9cc12ed34697a4e
Aleksandr Schemelev <prophreaker@gmail.com>
2021-04-06 17:19:33 +0300

    feat: add insertion sort

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../utils/utils.h"
+
+void insertion_sort(std::vector<int> &V) {
+    std::cout << "insertion sort" << std::endl;
+
+    int N = int(V.size());
+
+    for (int pos = 1; pos < N; pos++) {
+        auto i = pos;
+
+        while (i > 0 and V[i - 1] > V[i]) {
+            std::swap(V[i - 1], V[i]);
+            i -= 1;
+        }
+
+        print_vector(V);
+    }
+}
+
+int main() {
+    std::vector<int> V;
+
+    V = input_vector();
+    print_vector(V);
+    insertion_sort(V);
+
+    if (is_ascending_order(V)) {
+        std::cout << "ascending\n";
+    } else {
+        std::cout << "not ascending";
+    }
+
+    return 0;
+}
